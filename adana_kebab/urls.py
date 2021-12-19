6b41a3482f0e836ed187dc1d08cd24a045c7f0ce
Sardorbek3007 <ergashbekovsardorbek@gmail.com>
2021-12-19 13:01:48 +0500

    newapi

--- adana_kebab/urls.py
+++ adana_kebab/urls.py
@@ -62,7 +62,7 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('menu/all/', get_menu),
-    path('special/all/', get_menu)
+    path('special/all/', get_specialmenu)
 
 ]
 
--- kebab/views.py
+++ kebab/views.py
@@ -13,6 +13,44 @@ from rest_framework.decorators import api_view, schema
 import json
 from django.http import JsonResponse
 
+
+@api_view(['POST'])
+def add_cart(request):        
+    data = request.data
+
+    user_id = data["user_id"]
+    user = get_user_model().objects.get(id=user_id)
+    products = data["products"]
+    card = None
+    result = {}
+    try:
+        card = Card.objects.get(user=user)      
+    except Card.DoesNotExist:
+        card = Card.objects.create(user=user)
+    result = {}
+    result["user_id"] = user_id
+    result["cart_id"] = card.id
+    result["cart_items"] = []
+    
+    for product in products:    
+        product_id = product["product_id"]        
+        total = product["total"]          
+
+        product = Menyu.objects.get(id=product_id)  
+        try:
+            card_item = CardItem.objects.get(product=product, card=card)
+            card_item.total = total
+            
+        except CardItem.DoesNotExist:
+            card_item = CardItem.objects.create(
+                product = product,
+                card = card,
+                total = total
+            )
+        card_item.save()    
+        result["cart_items"].append({"id": card_item.id, "total": card_item.total, "product": card_item.product.id})                    
+    return JsonResponse(result)
+
 @api_view(['GET'])
 def get_menu(request):
     menus = Menyu.objects.all()
@@ -23,7 +61,7 @@ def get_menu(request):
     return JsonResponse(datas, safe=False)
     
 @api_view(['GET'])
-def get_menu(request):
+def get_specialmenu(request):
     menul = Special.objects.all()
     datas = list(menul.values())
     for data in datas:
